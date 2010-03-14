02965f6c295a5d8a803efd2f3dc3e5fe25403b85
annathakumaran <ananthakumaran@gmail.com>
2010-03-14 23:50:35 +0530

    refactoring to more functional style

--- problem5.scala
+++ problem5.scala
@@ -8,14 +8,9 @@ object problem {
   *  What is the smallest number that is evenly divisible by all of the numbers from 1 to 20?
   */ 
   
-  def smallNumber(limit:Int):Int = {
-		var result = limit
-		(2 until limit).foreach( (x:Int) => result = lcm( x , result))
-		result
-  }
-  
-   def lcm(a:Int,b:Int) = a*b/gcd(a,b)
-   def gcd(a:Int,b:Int):Int = if (b == 0) a else gcd(b,a%b)
+	def smallNumber(limit:Int) = (2 until limit).reduceLeft(lcm(_,_))
+	def lcm(a:Int,b:Int) = a*b/gcd(a,b)
+	def gcd(a:Int,b:Int):Int = if (b == 0) a else gcd(b,a%b)
 }
 
 assert(problem.smallNumber(10) == 2520)
