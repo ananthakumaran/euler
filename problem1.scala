597e44f11dc892c8fa8f296e1ac4ecbe06e41aab
annathakumaran <ananthakumaran@gmail.com>
2010-03-14 15:46:29 +0530

    cleaning up the problem 1

--- problem1.scala
+++ problem1.scala
@@ -2,10 +2,6 @@ object problem {
    /** If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
      * Find the sum of all the multiples of 3 or 5 below 1000.
 	 **/
-	def main(args: Array[String]) {
-	 assert(sumOfMultiples(10) == 23)
-	 print(sumOfMultiples(1000))
-   }
   
   def sumOfMultiples(limit:Int):Int = {
      var result = 0
@@ -13,4 +9,7 @@ object problem {
 	 result
   }
 }
-problem.main(null)
+
+
+assert( problem.sumOfMultiples(10) == 23)
+assert( problem.sumOfMultiples(1000) == 233168)
