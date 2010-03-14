ae6cf72e1df8b836dc6fe9399f6057b5ee260dd6
annathakumaran <ananthakumaran@gmail.com>
2010-03-14 10:58:47 +0530

    problem 3

---
+++ problem3.scala
@@ -0,0 +1,29 @@
+/**
+ *  @author ananth
+ */
+object problem {
+  /**
+   * The prime factors of 13195 are 5, 7, 13 and 29.
+   *
+   *  What is the largest prime factor of the number 600851475143 ?
+   **/
+
+   def largestPrimeFactor(number:Long):Long = {
+    var n = number
+	var i = 2L
+	while(i <= n) 
+	{
+	  if( n % i == 0)
+	  {
+	    n = n/i
+		if(n == 1)
+		{
+		   return i
+		}
+	  }
+	  i += 1   
+	}
+	number
+   }
+}
+
