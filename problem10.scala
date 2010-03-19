c60aa59febe006517a1558924893d329493d0654
annathakumaran <ananthakumaran@gmail.com>
2010-03-19 10:10:16 +0530

    oops

--- problem10.scala
+++ problem10.scala
@@ -13,25 +13,10 @@ object Problem {
 	
 	def prime(limit:Int) = {
 		 val sieve = new Array[Boolean](limit)
-		 for(i <- 2 until limit ; if(!sieve(i) && isPrime(i))){
+		 for(i <- 2 until limit ; if(!sieve(i))){
 			    (2*i until (limit,i)).foreach(sieve(_) = true )
 		 }
 		(2 until limit).filter(!sieve(_)).foldLeft(0L)(_+_)
-	}
-	
-	def isPrime(n:Long):Boolean = {
-	    if(n < 2) return false
-        if(n == 2 || n == 3) return true
-        if(n%2 == 0 || n%3 == 0) return false
-        val sqrtN = Math.sqrt(n)+1
-        var i = 6L
-		while( i <= sqrtN )
-		{
-			if(n%(i-1) == 0 || n%(i+1) == 0) 
-				return false
-			i += 6	
-		}
-		true	
 	}
 }
 assert(Problem.prime(10) == 17)
